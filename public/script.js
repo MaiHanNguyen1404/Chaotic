683d9013a11622944a996cb55d01e99c9ac6694a
Mai Hân <s3927003@rmit.edu.vn>
2024-04-30 15:43:57 +1000

    update

--- public/script.js
+++ public/script.js
@@ -1,22 +1,70 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-const cnv = document.getElementById (`cnv_element`)
+// Get 'shapes' canvas elements
+const cnv = document.getElementById (`shapes`)
+
+// Setting canvas sizze
 cnv.width = innerWidth
 cnv.height = innerHeight
 
+// Get canvas context
 const ctx = cnv.getContext (`2d`)
 
-const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
+// Draw a root that grows at random speed and direction
+// Create a class for the root
+class Root {
+   constructor (x,y){
+      // 
+      this.letters = ["c", "l", "i", "c", "k"];
 
-   requestAnimationFrame (draw_frame)
+      //
+      this.randomLetter = this.letters[Math.floor (Math.random() * this.letters.length)]
+
+      this.x = x;
+      this.y = y;
+
+      // Define random direction between -4 and 4 
+      this.dX = Math.random() * 8 - 4;
+      this.dY = Math.random() * 8 -4;
+
+      // Define the initial length of the root
+      this.size = 12;
+
+      //
+      this.maxSize = 30;
+   }
+
+   // Define a function for the grow animation
+   grow(){
+      // Move the position of the root 
+      // according to the direction
+      this.x += this.dX
+      this.y += this.dY
+
+      // Increase the length of the root
+      this.size += 0.25
+
+      // Draw a segment as long as the length is less than 50
+      if (this.size < this.maxSize){
+         ctx.font = `${this.size}px monospace`;
+
+         ctx.fillStyle = `hsl(${Math.random() * 360}, 50%, 70%)` 
+
+         ctx.fillText (this.randomLetter, this.x, this.y)
+
+         requestAnimationFrame (this.grow.bind(this));
+      }
+   }
 }
 
-draw_frame ()
+// Define a function to handle mouse event
+// when the cursor moves over the canvas
+window.addEventListener('mousemove', function(e) {
+   // Draw new root 
+   // in cordinate with the position of the cursor
+   const root = new Root (e.x, e.y);
 
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
+   // Call the grow function
+   root.grow();
+})
